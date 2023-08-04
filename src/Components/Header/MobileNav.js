e27433e241cb6aa5a3aa6268a8827d74e82317f2
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2023-08-04 21:39:14 +0300

    start up mobile nav

--- src/Components/Header/MobileNav.js
+++ src/Components/Header/MobileNav.js
@@ -2,9 +2,25 @@ import styles from "./MobileNav.module.scss";
 
 const MobileNav = () => {
   return (
-    <nav className={styles.mobileNav}>
-      <img src={require("../images/icon-menu.svg").default} alt="icon menu" />
-    </nav>
+    <>
+      <nav className={styles.mobileNav}>
+        <img
+          className={styles.openMenuIcon}
+          src={require("../images/icon-menu.svg").default}
+          alt="icon menu"
+        />
+      </nav>
+
+      <div className={styles.overlay}></div>
+
+      <div className={styles.navMenu}>
+        <img
+          className={styles.closeMenuIcon}
+          src={require("../images/icon-close-menu.svg").default}
+          alt="icon close"
+        />
+      </div>
+    </>
   );
 };
 
